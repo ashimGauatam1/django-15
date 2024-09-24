115418e6145b5cd1b31132e23529dcc9ad619973
ashim <ashimgautam01@gmail.com>
2024-09-24 21:24:06 +0545

    day 7

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
 # Create your models here.
+
+class Blogs(models.Model):
+    title=models.CharField(max_length=200,blank=False,)
+    subtitle=models.CharField(max_length=100,blank=False,null=False)
+    description=models.TextField()
+    image=models.ImageField(upload_to="images",blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+
